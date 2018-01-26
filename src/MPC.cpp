892711044de83165b2ff0f591322c6494ec99b06
scifiss <rebecca.shuang@gmail.com>
2018-01-26 13:08:00 -0600

    Add files via upload

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -27,7 +27,7 @@ const double Lf = 2.67;
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
 // when one variable starts and another ends to make our lifes easier.
-size_t N = 12;
+size_t N = 8;
 double dt = 0.15; //0.12;
 size_t x_start = 0;
 size_t y_start = x_start + N;  //N
